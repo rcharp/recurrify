9e92352a1fe87b7ef6bb8e6b550f18c8833a9686
ricky <rickycharpentier@gmail.com>
2020-10-22 16:06:56 -0400

    removed keys

--- app/blueprints/shopify/config.py
+++
@@ -1,9 +0,0 @@
-SHOPIFY_API_KEY = "2c223335de3f2c3b822ec17c28fca9ef"
-SHOPIFY_SECRET = "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"
-
-APP_NAME = "Recurrify"
-SERVER_HOSTNAME = "7ef7f9b6218f.ngrok.io"
-SERVER_BASE_URL = "https://" + SERVER_HOSTNAME
-INSTALL_REDIRECT_URL = SERVER_BASE_URL + "/"
-
-WEBHOOK_APP_UNINSTALL_URL = "https://" + SERVER_HOSTNAME + "/auth/callback"
