028268feae8e9cec99c40ed522c2255d3a9d308e
ricky <rickycharpentier@gmail.com>
2020-10-24 20:51:34 -0400

    changes

--- app/blueprints/user/views.py
+++ app/blueprints/user/views.py
@@ -124,6 +124,8 @@ def signup(shop_id=None, email=None, url=None):
             if login_user(u):
                 # Set the user id on the shop
                 s = Shop.query.filter(Shop.shop == url).scalar()
+                a = Shop.query.all()
+                print(a)
                 print(s)
                 if s is not None:
                     s.user_id = u.id
